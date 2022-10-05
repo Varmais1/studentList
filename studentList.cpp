20cb179d16b607be17971a2268722ab2fa736233
Varmais1 <418487@bsd48.org>
2022-10-05 14:26:00 -0700

    programmer adding student not working

--- studentList.cpp
+++ studentList.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <cstring>
 /* Name: Ishaan Varma
 What this file does: Has everything for the student list project
 Date: 10/5/2022
@@ -12,7 +14,7 @@ struct Student {
   float GPA;
 };
 
-Student* newStudent() {
+Student* userAddNewStudent() {
   Student* newLearner = new Student();
   cout << "Type in the student's first name." << endl;
   cin.get(newLearner->firstName, 15);
@@ -27,11 +29,22 @@ Student* newStudent() {
   return newLearner;
 }
 void displayStudent(Student* student);
-
+Student* newStudent(char firstName[], char lastName[], int id, float GPA);
 
 int main() {
-  Student* student = newStudent();
-  displayStudent(student);
+  //Student* student = userAddNewStudent();
+  //displayStudent(student);
+  char fname[15];
+  char lname[15];
+  strcpy(fname, "Ishaan");
+  strcpy(lname, "Varma");
+  Student* student2 = newStudent(fname,lname,418487,4.00);
+  displayStudent(student2);
+
+
+
+  delete student2;
+  return 0;
 }
 
 void displayStudent(Student* student) {
@@ -39,4 +52,13 @@ void displayStudent(Student* student) {
   cout << "Last name: " << student->lastName << endl;
   cout << "Student ID: " << student->id << endl;
   cout << "Student's GPA: " << student->GPA << endl;
+}
+
+Student* newStudent(char firstName[15], char lastName[15], int id, float GPA) {
+  Student* newLearner = new Student();
+  newLearner->firstName = firstName;
+  newLearner->lastName = lastName;
+  newLearner->id = id;
+  newLearner->GPA = GPA;
+  return newLearner;
 }
